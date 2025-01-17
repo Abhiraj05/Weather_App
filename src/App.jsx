3edd4eb9b1c166e550cf789d3ef21e0f07dd3100
Abhiraj Shilkar <93819088+Abhiraj05@users.noreply.github.com>
2025-01-17 11:02:02 +0530

    files added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='flex text-center items-center min-h-screen'>
+      <div>this  is weather app</div> 
+    </div>
+    </>
+  )
+}
+
+export default App
